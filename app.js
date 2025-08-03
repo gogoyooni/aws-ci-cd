9b0297ba7540948656f8c5b56caf1dfa428c32e4
gogoyooni <taeyoondev@gmail.com>
2025-08-03 18:31:36 +0900

    changed app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 
 // 누군가 우리 웹사이트의 가장 기본 주소('/')로 접속하면 실행되는 부분입니다.
 app.get("/", (req, res) => {
-  res.send("Hello, DevOps with Express and Jenkins!! 333ß");
+  res.send("Hello, DevOps with Express and Jenkins!! 44");
 });
 
 // 8080 포트에서 웹서버를 시작하고, 성공하면 메시지를 보여줍니다.
